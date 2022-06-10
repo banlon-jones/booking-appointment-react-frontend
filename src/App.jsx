73065df9470ccf78452a89faea9e29fa7068aed2
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-06-10 13:37:46 +0300

    Create the navigation panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/navbar/Nav';
 import Views from './components/views/Views';
 
-const App = () => {
-  return <div><Views /></div>;
-};
+const App = () => (
+    <Routes>
+      { <Route path='/' element={<Nav />}/>}
+      { <Route path='/' element={<Views />} /> }
+      {/* <Route exact path='/main' element={<Main />} /> */}
+      {/* <Route exact path='/myreservations' element={<MyReservations />} /> */}
+      {/* <Route exact path='/addItem' element={<AddItem />} /> */}
+      {/* <Route exact path='/deleteItem' element={<DeleteItem />} /> */}
+      {/* <Route exact path='/reserve' element={<ReserveItem />} /> */}
+    </Routes>
+);
 
 export default App;
