57d9484f4446c958f411b8bbd9a2824fbf609849
Adorn Choga <adornchoga@gmail.com>
2022-06-10 15:00:31 +0200

    Add user role state

--- src/store/user/user.js
+++ src/store/user/user.js
@@ -1,9 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const setRole = createAsyncThunk('setRole', async () => {
+  if (sessionStorage.getItem('JwtAccessToken')) {
+    const url = 'http://127.0.0.1:3000/user-role';
+    try {
+      const { data } = await axios.post(
+        url,
+        {},
+        {
+          headers: {
+            Authorization: sessionStorage.getItem('JwtAccessToken'),
+          },
+        },
+      );
+      return data.role;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return '';
+});
 
 const userSlice = createSlice({
   name: 'user',
   initialState: {
     loggedIn: sessionStorage.getItem('JwtAccessToken') !== null,
+    role: '',
   },
   reducers: {
     logInUser(state) {
@@ -14,7 +37,11 @@ const userSlice = createSlice({
       return { ...state, loggedIn: false };
     },
   },
+  extraReducers: {
+    [setRole.fulfilled]: (state, { payload }) => ({ ...state, role: payload }),
+  },
 });
 
 export const { logInUser, logOutUser } = userSlice.actions;
+export { setRole };
 export default userSlice.reducer;
