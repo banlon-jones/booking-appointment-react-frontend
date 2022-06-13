89d5524bdce5aebb47227d34d5c4e4389d7b12b5
tYohannes <teklegy@gmail.com>
2022-06-13 16:25:53 +0300

    Add resortsSlice in redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './user/user';
+import resortsSlice from './resorts/resortsSlice';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
+    resorts: resortsSlice,
   },
 });
 
