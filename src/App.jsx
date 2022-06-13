e6ddf0578485bf69bf4dc5e8a528a411e263f394
tYohannes <teklegy@gmail.com>
2022-06-13 11:55:57 +0300

    Fix linter error on App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Views from './components/views/Views';
 
-const App = () => {
-  return <div><Views /></div>;
-};
+const App = () => <div><Views /></div>;
 
 export default App;
