f4c7d62eb593e19f41ee5b39f6eba45c6b2cb657
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-06-13 12:47:40 +0300

    Fix merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import Nav from './components/navbar/Nav';
 import Views from './components/views/Views';
 
 const App = () => (
-  <div><Nav /><Views /></div>
+  <div>
+    <Nav />
+    <Views />
+  </div>
 );
 
 export default App;
