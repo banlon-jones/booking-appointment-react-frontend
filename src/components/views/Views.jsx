c917dfa74ab7e7c081a4fc25af5d1c621f23b980
tYohannes <teklegy@gmail.com>
2022-06-14 10:41:22 +0300

    Add resortTodelete route

--- src/components/views/Views.jsx
+++ src/components/views/Views.jsx
@@ -4,12 +4,14 @@ import Resorts from '../home/Resorts';
 import ResortDetails from '../home/ResortDetails';
 import LogIn from '../login/LogIn';
 import SignUp from '../signup/SignUp';
+import ResortsToDelete from '../home/ResortsToDelete';
 
 const Views = () => (
   <Routes>
     <Route index element={<Resorts />} />
     <Route path="/resorts" element={<Resorts />} />
     <Route path="/resorts/:id" element={<ResortDetails />} />
+    <Route path="/deleteResorts" element={<ResortsToDelete />} />
     <Route path="/sign-up" element={<SignUp />} />
     <Route path="/login" element={<LogIn />} />
   </Routes>
