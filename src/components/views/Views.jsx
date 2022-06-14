774345bf27b1ed08758cd6cc49cb04a4abba5ff0
tYohannes <teklegy@gmail.com>
2022-06-14 09:26:14 +0300

    Create Resorts component

--- src/components/views/Views.jsx
+++ src/components/views/Views.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Resorts from '../home/Resorts';
+import ResortDetails from '../home/ResortDetails';
 import LogIn from '../login/LogIn';
 import SignUp from '../signup/SignUp';
 
 const Views = () => (
   <Routes>
     <Route index element={<Resorts />} />
+    <Route path="/resorts" element={<Resorts />} />
+    <Route path="/resorts/:id" element={<ResortDetails />} />
     <Route path="/sign-up" element={<SignUp />} />
     <Route path="/login" element={<LogIn />} />
   </Routes>
