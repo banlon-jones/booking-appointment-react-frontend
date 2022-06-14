21a099ee60f16daefd4c82f3b1801e3201289ba1
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-06-14 22:25:43 +0300

    fix linter in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from './components/navbar/Nav';
 import Views from './components/views/Views';
-import './App.css'
+import './App.css';
 
 const App = () => (
   <div>
